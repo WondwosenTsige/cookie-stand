4503fad5b1793403410e9c6da42536365b814918
Wondwosen Tsige <wondneh@gmail.com>
2020-11-04 17:51:58 -0800

    When this commit is applied, I can test if I try to add a new location. It worked

--- js/app.js
+++ js/app.js
@@ -7,34 +7,50 @@ function getRandomIntInclusive(min, max) {
     return Math.floor(Math.random()*(max-min +1) +min);
 }
 
-store.operationHour = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+var operationHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
-store.tableContent = document.getElementById("Store-list-table");
+var tableContent = document.getElementById("Store-list-table");
 
 
-store.allStoreInfo = [];
+Store.allStoreInfo = [];
 
-function store (storeLocation, minCustomerPerHour, maxCustomerPerHour, avgCookiesSoldPerCustomer) {
+function Store (storeLocation, minCustomerPerHour, maxCustomerPerHour, avgCookiesSoldPerCustomer) {
   
   this.storeLocation = storeLocation;
   this.minCustomerPerHour = minCustomerPerHour;
   this.maxCustomerPerHour = maxCustomerPerHour;
   this.avgCookiesSoldPerCustomer = avgCookiesSoldPerCustomer;
   this.totalCookiesPerDay = 0;
-  this.cookiesPerHr = [];
-  store.allStoreInfo.push(this);
+  this.cookiesPerHour = [];
+  this.customerPerHour =[];
+  Store.allStoreInfo.push(this);
 
   
 };
 
-store.prototype.getCustomerPerHour = function () {
+Store.prototype.getCustomerPerHour = function () {
 
 
     for (var i = 0; i < operationHours.length; i++) {
         var randomNumberOfCustomers = getRandomIntInclusive(this.minCustomerPerHour, this.maxCustomerPerHour);
         this.customerPerHour.push(randomNumberOfCustomers);
 
-    }
+    };
+}
+    Store.prototype.getCookiesPerHour =function () {
+
+        for (var i = 0; i < operationHours.length; i++) {
+            var cookiesSoldEachHour = this.customerPerHour[i] * this.avgCookiesSoldPerCustomer;
+            this.cookiesPerHour.push(Math.round(cookiesSoldEachHour));                              
+            this.totalCookiesPerDay = this.totalCookiesPerDay + Math.round (cookiesSoldEachHour); 
+        }
 
 }
 
+var seattle = new Store('Seattle', 23, 65, 6.3);
+seattle.getCustomerPerHour();
+seattle.getCookiesPerHour();
+
+var tokyo = new Store('Tokyo', 28, 71, 4.3);
+tokyo.getCustomerPerHour();
+tokyo.getCookiesPerHour();
