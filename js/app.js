f9f037c894d8ab6da5a43ebc623bb1009819df07
Wondwosen Tsige <wondneh@gmail.com>
2020-11-04 16:57:16 -0800

    When this commit is applied, it will create a prototype function in lab07

--- js/app.js
+++ js/app.js
@@ -1,12 +1,18 @@
 'use strict';
 
+function getRandomIntInclusive(min, max) {
+
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random()*(max-min +1) +min);
+}
+
 store.operationHour = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
 store.tableContent = document.getElementById("Store-list-table");
 
 
 store.allStoreInfo = [];
-store.totalsales = 0;
 
 function store (storeLocation, minCustomerPerHour, maxCustomerPerHour, avgCookiesSoldPerCustomer) {
   
@@ -14,11 +20,21 @@ function store (storeLocation, minCustomerPerHour, maxCustomerPerHour, avgCookie
   this.minCustomerPerHour = minCustomerPerHour;
   this.maxCustomerPerHour = maxCustomerPerHour;
   this.avgCookiesSoldPerCustomer = avgCookiesSoldPerCustomer;
-  this.totalCookiesPerDay = [];
-  this.cookiesPerHr = 0;
-  this.allStoreInfo.push(this);
+  this.totalCookiesPerDay = 0;
+  this.cookiesPerHr = [];
+  store.allStoreInfo.push(this);
 
   
-}
+};
 
+store.prototype.getCustomerPerHour = function () {
+
+
+    for (var i = 0; i < operationHours.length; i++) {
+        var randomNumberOfCustomers = getRandomIntInclusive(this.minCustomerPerHour, this.maxCustomerPerHour);
+        this.customerPerHour.push(randomNumberOfCustomers);
+
+    }
+
+}
 
